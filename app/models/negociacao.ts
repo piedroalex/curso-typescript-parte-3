154d01e123764aa7255713d5b2a02a23aa39db2f
Pedro Alex L Martins <piedroalex@gmail.com>
2021-07-15 17:26:16 -0300

    Adicionando métodos estáticos, repassando a conversão de negociação para o modelo Negociacao e configurando parâmetros opcionais na View.

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -14,4 +14,12 @@ export class Negociacao {
         const data = new Date(this._data.getTime());
         return data;
     }
+
+    public static criaDe(dataParam: string, quantidadeParam: string, valorParam: string): Negociacao {
+        const exp = /-/g;
+        const data = new Date(dataParam.replace(exp, ','));
+        const quantidade = parseInt(quantidadeParam);
+        const valor = parseFloat(valorParam);
+        return new Negociacao(data, quantidade, valor);
+    }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -11,4 +11,11 @@ export class Negociacao {
         const data = new Date(this._data.getTime());
         return data;
     }
+    static criaDe(dataParam, quantidadeParam, valorParam) {
+        const exp = /-/g;
+        const data = new Date(dataParam.replace(exp, ','));
+        const quantidade = parseInt(quantidadeParam);
+        const valor = parseFloat(valorParam);
+        return new Negociacao(data, quantidade, valor);
+    }
 }

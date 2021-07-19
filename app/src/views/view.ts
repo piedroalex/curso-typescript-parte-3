1ac9d068ec11e1d3c629f327e61a2b9cdd133744
Pedro Alex L Martins <piedroalex@gmail.com>
2021-07-19 00:41:41 -0300

    Adicionando um decorator para calcular o tempo de execução dos métodos.

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
@@ -17,6 +19,7 @@ export abstract class View<T> {
 
     protected abstract template(model: T): string;
 
+    @logarTempoDeExecucao()
     public update(model: T): void {
         let template = this.template(model);
 
